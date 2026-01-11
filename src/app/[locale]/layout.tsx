ba5b9ff8d9762ee24e09a88137f102f6c8baa473
tntruong96 <nomad@brickmate.vn>
2026-01-11 12:34:02 +0700

    update layout file

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -41,7 +41,7 @@ export default async function LocaleLayout({
   params,
 }: {
   children: React.ReactNode;
-  params: { locale: string };
+  params: Promise<{ locale: string }>;
 }) {
   // Ensure that the incoming `locale` is valid
   const { locale } = await params;
